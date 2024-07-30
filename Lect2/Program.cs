cd214caeafb646d63cc4ee3abe323313441ae7ca
Tarek Mamdouh <egypty3@gmail.com>
2024-07-30 23:21:21 +0300

    How can the user interact with the program

--- Lect2/Program.cs
+++ Lect2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number1,number2,number3;
+
+Console.WriteLine("Please, input first number :");
+number1 =  int.Parse( Console.ReadLine());
+
+Console.WriteLine("Please, input second number :");
+number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please, input third number :");
+number3 = int.Parse(Console.ReadLine());
+
+int result = number1 * number2 * number3;
+
+//Console.WriteLine(
+//	"Result of : {0} x  {1} x {2} = {3}" ,
+//	number1,number2,number3,result);
+
+Console.WriteLine(
+	$" Result of : {number1} x {number2} x {number3} = {result}"
+	);
+
