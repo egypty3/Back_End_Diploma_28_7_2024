2d31138152f41e32337d4fdc1777c2dae69f9742
Tarek Mamdouh <egypty3@gmail.com>
2024-07-30 21:54:29 +0300

    Add project files.

---
+++ Lect1/Program.cs
@@ -0,0 +1,19 @@
+﻿string userName = "Mohamed El Sharkawi";
+Console.WriteLine("Hello " + userName);
+Console.WriteLine("5 + 7 + 8 = " + (5 + 7 + 8));
+Console
+	.WriteLine(
+	"Masaa " +
+	"El kheer"
+	);
+// integer division
+Console.WriteLine(5 / 2);
+
+// floating point division
+Console.WriteLine(5.0 / 2.0);
+// percedence for the * , / operators
+// over + , - operators
+Console.WriteLine(-1 + 4 * 6);
+// perecedence would be for (,)
+// over any operators
+Console.WriteLine((-1 + 4) * 6);
