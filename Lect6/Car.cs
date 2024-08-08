c6bc819b6bfb6c20bc62f98b07e79a70aff9aae0
Tarek Mamdouh <egypty3@gmail.com>
2024-08-08 22:17:15 +0300

    + Car Class + class fields + class methods + contructor + constuctor overloads +  class diagram

---
+++ Lect6/Car.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lect6
+{
+	internal class Car
+	{
+		public string make;
+		public string model;
+		public int yearOfManifucture;
+		public string color;
+		public int maxSpeed;
+
+		// constructor is a special method :
+		// 1. Cause it's name is the same as the name of the Class
+		// 2. It doesn't return anything even void !!!
+
+		// default constructor is created automatically if no other constructor overloads exist
+		public Car()
+		{
+
+		}
+
+		// constructor overload
+		public Car( string make, string model, 
+			int yearOfManifucture, string color, int maxSpeed)
+        {
+			this.make = make;
+			this.model = model;
+			this.yearOfManifucture = yearOfManifucture;
+			this.color = color;
+			this.maxSpeed = maxSpeed;
+		}
+        public string GetCarInfo()
+		{		
+			return 
+				$"Make: {make}, Model: {model}, " +
+				$"Year: {yearOfManifucture}, Color: {color}, Max Speed: {maxSpeed}";
+		}
+
+		public decimal DetermineMarketValue()
+		{
+			decimal carPrice;
+			if (yearOfManifucture < 2002)
+			{
+				carPrice = 20000;
+			}
+			else
+			{
+				carPrice = 300000;
+			}
+			// Someday I might look up for the market value 
+			// using more accurate online WebAPI
+			return carPrice;
+		}
+	}
+}
---
+++ Lect6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lect6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// object initialization
+			Car car1 = new Car();
+			car1.color = "black";
+			car1.make = "Toyota";
+			car1.model = "Corolla";
+			car1.yearOfManifucture = 2005;
+			car1.maxSpeed = 220;
+
+			Console.WriteLine(car1.GetCarInfo());
+
+			// object initialization
+			Car car2 = new Car("Kia", "Cerato", 2006, "blue", 200);
+			Console.WriteLine(car2.GetCarInfo());
+			Console.WriteLine(car2.DetermineMarketValue());
+		}
+	}
+}
+
